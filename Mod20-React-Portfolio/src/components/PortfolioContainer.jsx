77aa3b5bf01d5a78ca5bb07923fae5864d6d8fd8
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-04-20 16:42:08 -0700

    Add background image to header

--- Mod20-React-Portfolio/src/components/PortfolioContainer.jsx
+++ Mod20-React-Portfolio/src/components/PortfolioContainer.jsx
@@ -4,6 +4,7 @@ import Resume from './pages/Resume';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import '../index.css';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About Me');
