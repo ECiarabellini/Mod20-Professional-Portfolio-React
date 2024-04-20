ebc78d872ee9038e85e6850fbb3a04d31b25f347
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-04-20 16:05:49 -0700

    Update About Me

--- Mod20-React-Portfolio/src/components/pages/About.jsx
+++ Mod20-React-Portfolio/src/components/pages/About.jsx
@@ -1,14 +1,12 @@
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      I am an experienced professional with a degree in Business Leadership from the University of Puget Sound. Over the past 11 years, I have built a robust foundation in financial operations, gaining valuable insights into industry intricacies. In my recent role, I have spent two years contributing to the success of a growing education and upskilling benefits company, focusing on operational efficiencies, automation, and process and people management.
+      </p>
+      <p>
+      Now, I am eager to pivot my career to software engineering, driven by a passion for technology and automation. My career experience has equipped me with a strategic mindset and problem-solving skills, which I believe will be valuable assets in the dynamic field of software development. I am excited about the prospect of applying my business acumen to contribute to the ever-evolving world of technology. With a commitment to continuous learning and adaptability, I am ready to embrace the challenges and opportunities that come with this transition, bringing a unique blend of business expertise and a fresh perspective to the field.
       </p>
     </div>
   );
